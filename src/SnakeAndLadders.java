31898a608193071d615bf1d9e31668177c47f642
imsolanki <shobhitsingh.e28@gmail.com>
2021-03-09 01:34:15 +0530

    Snake and Ladders with new implemented functionalities.

--- src/SnakeAndLadders.java
+++ src/SnakeAndLadders.java
@@ -1,16 +1,31 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
-public class SnakeAndLadders extends  JFrame {
-
+public class SnakeAndLadders extends  JFrame implements ActionListener{
+    JButton btn;
+    JLabel label;
+    int location=1;
     public void showFrame(){
-        super.setBounds(180, 150, 480, 480);
+
+        super.setPreferredSize(new Dimension(500, 520));
+        super.setResizable(false);
         super.setVisible(true);
         ImageIcon imageIcon = new ImageIcon("C:\\Users\\Owner\\Desktop\\name.png"); // load the image to a imageIcon
         Image image = imageIcon.getImage(); // transform it
-        Image newimg = image.getScaledInstance(480, 480,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way
+        Image newimg = image.getScaledInstance(480, 450,  0); // scale it the smooth way
         imageIcon = new ImageIcon(newimg);
-        super.add(new JLabel(imageIcon));
+        label = new JLabel(imageIcon);
+        label.setBounds(0,0,480,450);
+        super.add(label);
+        btn = new JButton();
+        btn.setBounds(245,455,75,25);
+        super.add(btn);
+        btn.addActionListener(this);
+
+        super.setLayout(null);
         super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         super.pack();
 
@@ -19,14 +34,37 @@ public class SnakeAndLadders extends  JFrame {
     {
         super.paint(g);
         g.setColor(Color.BLACK);
-        int col,row;
-        col=453;
-        row=27;
-        g.fillOval(row,col,40,40);
+        int x;
+        int y;
+        //----------------------------------------------------------------//
+
+        //----------------------------------------------------------------//
+        g.fillOval(x,y,40,40);
     }
     public static void main(String[] args) {
         SnakeAndLadders s = new SnakeAndLadders();
         s.showFrame();
     }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if (e.getSource().equals(btn))//start
+        {
+
+            //int x= (int) Math.floor(Math.random() * (7 - 1) + 1);
+            Random r =new Random();
+            int x = r.nextInt(6)+1;
+            movements(x);
+            btn.setText(String.valueOf(x));
+        }
+
+        }
+        public void movements(int move){
+
+        location =location+move;
+
+        this.repaint();
+        }
+
 
 }
